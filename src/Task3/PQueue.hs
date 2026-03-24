e24c4844da4bdbab1ccf354c173b001747e671dc
ptntsv <hitogavax@ya.ru>
2026-03-24 23:19:28 +0700

    Task2 solved

--- src/Task3/PQueue.hs
+++ src/Task3/PQueue.hs
@@ -1,25 +1,24 @@
 {-# OPTIONS_GHC -Wall #-}
+
 -- The above pragma enables all warnings
 
 module Task3.PQueue where
 
 import Common.PriorityQueue
-
-import Task1 (Measured(..), MinMax(..))
+import Task1 (Measured (..), MinMax (..))
 import Task3.Tree
 
 -- * Priority queue definition
 
 -- | Priority queue based on binary tree
-newtype PQueue k v = PQueue { getTree :: Tree (MinMax k) (Entry k v) }
+newtype PQueue k v = PQueue {getTree :: Tree (MinMax k) (Entry k v)}
   deriving (Show, Eq)
-
 
 -- | Priority queue entry wrapper
-newtype Entry k v = Entry { getEntry :: (k, v) }
+newtype Entry k v = Entry {getEntry :: (k, v)}
   deriving (Show, Eq)
 
-instance Ord k => Measured (MinMax k) (Entry k v) where
+instance (Ord k) => Measured (MinMax k) (Entry k v) where
   measure = error "TODO: define measure (Measured (MinMax k) (Task3.Entry k v))"
 
 -- * Priority queue instance
@@ -29,5 +28,6 @@ instance PriorityQueue PQueue where
   toPriorityQueue = error "TODO: define toPriorityQueue (PriorityQueue Task3.PQueue)"
   entries = error "TODO: define entries (PriorityQueue Task3.PQueue)"
   insert = error "TODO: define insert (PriorityQueue Task3.PQueue)"
+  extractWith = error "TODO: define extractMin (PriorityQueue Task3.PQueue)"
   extractMin = error "TODO: define extractMin (PriorityQueue Task3.PQueue)"
   extractMax = error "TODO: define extractMax (PriorityQueue Task3.PQueue)"
--- src/Task4/PQueue.hs
+++ src/Task4/PQueue.hs
@@ -1,22 +1,22 @@
 {-# OPTIONS_GHC -Wall #-}
+
 -- The above pragma enables all warnings
 
 module Task4.PQueue where
 
 import Common.PriorityQueue
-
-import Task1 (Measured(..), MinMax(..))
+import Task1 (Measured (..), MinMax (..))
 import Task4.Tree
 
-newtype PQueue k v = PQueue { getTree :: Tree (MinMax k) (Entry k v) }
+newtype PQueue k v = PQueue {getTree :: Tree (MinMax k) (Entry k v)}
   deriving (Show, Eq)
 
 -- | Priority queue entry wrapper
-newtype Entry k v = Entry { getEntry :: (k, v) }
+newtype Entry k v = Entry {getEntry :: (k, v)}
   deriving (Show, Eq)
 
 -- | Measures given entry using both minimum and maximum priority 'k'
-instance Ord k => Measured (MinMax k) (Entry k v) where
+instance (Ord k) => Measured (MinMax k) (Entry k v) where
   measure = error "TODO: define measure (Measured (MinMax k) (Task4.Entry k v))"
 
 -- * Priority queue instance
@@ -26,5 +26,6 @@ instance PriorityQueue PQueue where
   toPriorityQueue = error "TODO: define toPriorityQueue (PriorityQueue Task4.PQueue)"
   entries = error "TODO: define entries (PriorityQueue Task4.PQueue)"
   insert = error "TODO: define insert (PriorityQueue Task4.PQueue)"
+  extractWith = error "TODO: define extractMin (PriorityQueue Task3.PQueue)"
   extractMin = error "TODO: define extractMin (PriorityQueue Task4.PQueue)"
   extractMax = error "TODO: define extractMax (PriorityQueue Task4.PQueue)"
