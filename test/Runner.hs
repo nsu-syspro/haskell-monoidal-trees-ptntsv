24fe0b60e4c0ed836842ef5c8ca1c047ccf60da5
ptntsv <hitogavax@ya.ru>
2026-03-26 21:34:44 +0700

    Testing: add some arguments for specifying test name

--- test/Runner.hs
+++ test/Runner.hs
@@ -1,32 +1,43 @@
+import Data.Data (Proxy (Proxy), Typeable)
+import Data.Maybe (fromMaybe)
 import Task1Suite (task1Checks, task1Tests)
 import Task2Suite (task2Checks, task2Tests)
 import Task3Suite (task3Checks, task3Tests)
 import Task4Suite (task4Checks, task4Tests)
 import Test.Tasty
+import Test.Tasty.Options (IsOption (..), OptionDescription (..))
+
+newtype TaskOpt = TaskOpt {getTaskOpt :: Maybe String}
+  deriving (Eq, Ord, Show, Typeable)
+
+instance IsOption TaskOpt where
+  defaultValue = TaskOpt Nothing
+
+  parseValue s = Just (TaskOpt (Just s))
+
+  optionName = pure "task"
+  optionHelp = pure "Run tests for a given task"
 
 main :: IO ()
--- main = defaultMain $ testGroup "" [checks, tests]
-main = defaultMain $ testGroup "" [task2All]
+main =
+  defaultMainWithIngredients
+    (includingOptions [Option (Proxy :: Proxy TaskOpt)] : defaultIngredients)
+    ( askOption $ \(TaskOpt task) ->
+        case task of
+          Just content -> askSuite content
+          _ -> testGroup "all" allSuites
+    )
 
-task2All :: TestTree
-task2All = testGroup "Task2 all" [task2Checks, task2Tests]
+askSuite :: String -> TestTree
+askSuite name = testGroup name (fromMaybe allSuites $ lookup name suitesByTask)
 
-checks :: TestTree
-checks =
-  testGroup
-    "Checks"
-    [ task1Checks,
-      task2Checks,
-      task3Checks,
-      task4Checks
-    ]
+allSuites :: [TestTree]
+allSuites = concatMap snd suitesByTask
 
-tests :: TestTree
-tests =
-  testGroup
-    "Tests"
-    [ task1Tests,
-      task2Tests,
-      task3Tests,
-      task4Tests
-    ]
+suitesByTask :: [(String, [TestTree])]
+suitesByTask =
+  [ ("Task1", [task1Tests, task1Checks]),
+    ("Task2", [task2Tests, task2Checks]),
+    ("Task3", [task3Tests, task3Checks]),
+    ("Task4", [task4Tests, task4Checks])
+  ]
